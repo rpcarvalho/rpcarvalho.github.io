18e18e28ee9e2a61f7bfde58537f5324d23f4b8c
rpcarvalho <rui.pf.carvalho@hotmail.com>
2017-05-16 01:29:17 +0100

    da

--- deUke/sketch.js
+++ deUke/sketch.js
@@ -1,5 +1,5 @@
 var cors = "https://crossorigin.me/";
-var url  = "https://ukulele-chords.com/get?ak=ad7602154409b93139d5624378066908&r=C&typ=sus2";
+var url  = "http://ukulele-chords.com/get?ak=ad7602154409b93139d5624378066908&r=C&typ=sus2";
 var xml;
 
 function setup() {
@@ -25,6 +25,14 @@ function xmlFun() {
 	print(name + diff + pic);
 	text(name, 10, 30);
 	text(diff, 80, 30);
-	var img = loadImage( pic);
-	image(img, 50, 50);
+
+	var img = new Image();
+	
+	//img.crossOrigin = "Anonymous";
+    img.src = pic;
+    img.onload= function() {
+		var ctx = canvas.getContext("2d");
+		ctx.drawImage(img, 20, 0, 152, 250, 50, 50, 152, 250);
+		};
 }
+
